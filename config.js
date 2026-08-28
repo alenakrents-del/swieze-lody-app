35388ca67f7e3defb9dfc6168da92ced2f580180
alenakrents-del <alenakrents@yandex.ru>
2026-08-28 15:54:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // We will add the public anon key + secure server function in the next step.
 window.APP_CONFIG = {
   supabaseUrl: "https://orabfrxuxvssdunkqnbx.supabase.co",
-  supabaseAnonKey: "",
-  mode: "demo"
+  supabaseAnonKey: "sb_publishable_8OxTZHP1CSD_hN8XvOw6QQ__Sz-_sv_",
+  mode: "live"
 };
